2aa5b40a10e5f6cd668ed68428b5dc795d698aa5
MatthewTRader <MatthewT_Rader@yahoo.com>
2015-04-02 01:13:10 -0500

    Home Slider Left & Right Arrows
    Added left and right arrows on the home slider.

--- template-home-page.php
+++ template-home-page.php
@@ -5,10 +5,12 @@
     <?php
     $myposts = get_posts('numberposts=15&category_name=Featured');
     foreach($myposts as $post) : ?>
-      <div class="homeSlider" style="background-image:url('<?php the_field('slide_image'); ?>');background-position:<?php the_field('position'); ?>;">
+    <div class="homeSlider">
+      <div class="homeSlides" style="background-image:url('<?php the_field('slide_image'); ?>');background-position:<?php the_field('position'); ?>;">
           <h2>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 		</h2>
+          </div>
   </div> 
 <?php endforeach; ?>
     </div>
