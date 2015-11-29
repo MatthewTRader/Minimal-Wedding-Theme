4b2a9b93fa5b75d14afdbd9115e86755559aecbb
MatthewTRader <MatthewT_Rader@yahoo.com>
2015-11-29 00:53:46 -0600

    Minor updates

---
+++ header-welcome.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title>Matthew T Rader | Dallas Photographer</title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="http://matthewtrader.com/wp-content/uploads/2013/02/Photographer-favicon.png" rel="shortcut icon">
+        <link href="http://matthewtrader.com/wp-content/uploads/2013/02/Photographer-favicon.png" rel="apple-touch-icon-precomposed">
+		<meta name="msvalidate.01" content="E93BA045C27D82721206AA9920042240" />
+                <meta name='yandex-verification' content='6fe1c40b06cdc1e1' />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel='stylesheet' href='http://matthewtrader.com/wp-content/themes/Minimal-Wedding-Theme/style.css'/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-19917703-7', 'auto');
+  ga('send', 'pageview');
+
+</script>
+        
+<link rel='stylesheet' id='jetpack_css-css'  href='http://matthewtrader.com/wp-content/plugins/jetpack/css/jetpack.css' media='all' />
+<script type='text/javascript' src='http://matthewtrader.com/wp-content/themes/Minimal-Wedding-Theme/js/lib/conditionizr-4.3.0.min.js'></script>
+<script type='text/javascript' src='http://matthewtrader.com/wp-content/themes/Minimal-Wedding-Theme/js/lib/modernizr-2.7.1.min.js'></script>
+<script type='text/javascript' src='http://matthewtrader.com/wp-includes/js/jquery/jquery.js'></script>
+<script type='text/javascript' src='http://matthewtrader.com/wp-includes/js/jquery/jquery-migrate.min.js'></script>
+<script type='text/javascript' src='http://matthewtrader.com/wp-content/themes/Minimal-Wedding-Theme/js/vendor/jquery.js'></script>
+<script type='text/javascript' src='http://matthewtrader.com/wp-content/themes/Minimal-Wedding-Theme/js/foundation.min.js'></script>
+<script type='text/javascript' src='http://matthewtrader.com/wp-content/themes/Minimal-Wedding-Theme/scripts.js'></script>
+</head>
+    
+	<body <?php body_class(); ?>>
+			<!-- /header -->
+  <div id="container"  class="small-12 columns container">
